897f65df811be3d8863a8c0e5d75f513cf13a098
Min-Hua Wu <mwu057@wch136-29.cs.ucr.edu>
2019-11-25 10:53:13 -0800

    11/25

--- mwu057_final/source/main.c
+++ mwu057_final/source/main.c
@@ -10,16 +10,22 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "io.h"
 #endif
 
-char Rows = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}
-char Cols = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F}
+char Rows[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}; // LED Matrix
+char Cols[] = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F}; // LED Matrix
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0xFF; PORTA = 0x00;
-    DDRC = 0x00; PORTC = 0xFF;
-    /* Insert your solution below */
+    DDRA = 0xFF; PORTA = 0x00; // LED Matrix 
+    DDRC = 0xFF; PORTC = 0x00; // LED Matrix
+    DDRB = 0xFF; PORTB = 0x00; // LCD data lines
+    DDRD = 0xFF; PORTD = 0x00; // LCD control lines
+    
+    LCD_init();
+    LCD_DisplayString(1,"Hello");
+
     while (1) {
        PORTA = Rows[0] | Rows[1];
        PORTC = Cols[0] & Cols[1];
