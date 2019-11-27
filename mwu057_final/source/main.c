d4f3bd1cbf8ade42a6da9a10790dd1a1743f40b5
Min-Hua Wu <mwu057@wch136-22.cs.ucr.edu>
2019-11-26 22:28:09 -0800

    update

--- mwu057_final/source/main.c
+++ mwu057_final/source/main.c
@@ -10,17 +10,38 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include <avr/epprom.h"
 #include "io.h"
 #endif
 
+#define up (~PINB & 0x08)
+#define left (~PINB & 0x10)
+#define down (~PINB & 0x20)
+#define right (~PINB & 0x40)
+
 char Rows[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80}; // LED Matrix
 char Cols[] = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F}; // LED Matrix
+
+enum MENU_STATES { WAIT, START, END } m_state ;
+
+void menu_tick() {
+   switch(m_state) { //transitions
+      case WAIT:
+         if (up && !down && !right && !left) {
+            m_state = START;
+         }
+         else if (up && left && !right && !down) {
+            m_state = WAIT;
+         }
+         break;
+      case START:
+         
 
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0xFF; PORTA = 0x00; // LED Matrix 
     DDRC = 0xFF; PORTC = 0x00; // LED Matrix
-    DDRB = 0xFF; PORTB = 0x00; // LCD data lines
+    DDRB = 0xE0; PORTB = 0x1F; // Output: LCD, speaker Input: buttons
     DDRD = 0xFF; PORTD = 0x00; // LCD control lines
     
     LCD_init();
